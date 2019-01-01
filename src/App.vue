0bd7bb4a626cfa67694185d711dcb8194edc30c5
Tomasz Olędzki <luksfer@poczta.onet.pl>
2019-01-01 11:53:58 +0100

    Hide Result if no input

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
       <Result
         @scroll="onResultScroll"
         :inputText="inputText"
+        v-if="Boolean(inputText)"
         class="result"
       />
     </main>
--- src/components/result/Result.vue
+++ src/components/result/Result.vue
@@ -2,53 +2,48 @@
   <section
     @scroll="onContentsScroll"
   >
-    <template v-if="inputText.length === 0">
-      <p>No kanji. No stats.</p>
-    </template>
-    <template v-if="inputText.length > 0">
-      <h2>Results</h2>
-      <p>
-        Following stats are based on
-        <select v-model="selectedKanjiDataset">
-          <option
-            v-for="(dataset, name, i) in kanjiDatasets"
-            :key="i"
-          >
-            {{ name }}
-          </option>
-        </select>
-        kanji frequency data<br/>
-        crafted by
-        <a href="https://github.com/scriptin">scriptin</a>
-        and gratefully grabbed from
-        <a href="https://github.com/scriptin/kanji-frequency">kanji-frequency</a>
-        (<a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>)
-      </p>
-      <p>
-        Found <strong>{{ result.foundKanjis.length }}</strong> distinct kanji.<br/>
-        To know <strong>80%</strong> of kanji in the provided text
-        you need to be familiar with <strong>{{ kanji80Percent }}</strong>
-        the most frequent kanji.<br/>
-        See the chart below for details.
-      </p>
-      <Chart
-        :kanjiInfos="result.kanjiInfos"
-      />
-      <p>
-        And here comes the list of {{ showCount }} kanji sorted by frequency.<br/>
-        Kanji found in the text are <strong>bolded</strong>.
-      </p>
-      <ul class="kanjis">
-        <li
-          v-for="(info, i) in result.kanjiInfos"
-          :class="['kanji', { found: info.found }]"
-          :key="info.kanji"
-          :title="getKanjiTitle(info, i)"
+    <h2>Results</h2>
+    <p>
+      Following stats are based on
+      <select v-model="selectedKanjiDataset">
+        <option
+          v-for="(dataset, name, i) in kanjiDatasets"
+          :key="i"
         >
-          {{ info.kanji }}
-        </li>
-      </ul>
-    </template>
+          {{ name }}
+        </option>
+      </select>
+      kanji frequency data<br/>
+      crafted by
+      <a href="https://github.com/scriptin">scriptin</a>
+      and gratefully grabbed from
+      <a href="https://github.com/scriptin/kanji-frequency">kanji-frequency</a>
+      (<a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>)
+    </p>
+    <p>
+      Found <strong>{{ result.foundKanjis.length }}</strong> distinct kanji.<br/>
+      To know <strong>80%</strong> of kanji in the provided text
+      you need to be familiar with <strong>{{ kanji80Percent }}</strong>
+      the most frequent kanji.<br/>
+      See the chart below for details.
+    </p>
+    <Chart
+      :kanjiInfos="result.kanjiInfos"
+    />
+    <p>
+      And here comes the list of {{ showCount }} kanji sorted by frequency.<br/>
+      Kanji found in the text are <strong>bolded</strong>.
+    </p>
+    <ul class="kanjis">
+      <li
+        v-for="(info, i) in result.kanjiInfos"
+        :class="['kanji', { found: info.found }]"
+        :key="info.kanji"
+        :title="getKanjiTitle(info, i)"
+      >
+        {{ info.kanji }}
+      </li>
+    </ul>
   </section>
 </template>
 
