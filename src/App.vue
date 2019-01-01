e22b8f8c58f43abe6bf30166d41a71d0f2418abe
Tomasz Olędzki <luksfer@poczta.onet.pl>
2019-01-01 11:46:03 +0100

    Extract App.scss, global.scss

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,8 @@
 </template>
 
 <script>
+import './App.scss';
+
 import Header from './components/header/Header.vue';
 import Input from './components/input/Input.vue';
 import Result from './components/result/Result.vue';
@@ -49,95 +51,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-@import url('../node_modules/normalize.css');
-
-$transitionDuration: .3s;
-
-body {
-  color: #222;
-  font-family: sans-serif;
-  padding: 0;
-  overflow: hidden;
-}
-
-h1, h2, p {
-  margin-top: 0;
-}
-
-p {
-  line-height: 1.45;
-}
-
-#app {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-
-.header {
-  background: #e26f6f;
-  border-bottom: 1px solid #999;
-  color: #fff;
-  height: 60px;
-  padding: 1em 20px;
-  transition:
-    height $transitionDuration,
-    padding-top $transitionDuration,
-    padding-bottom $transitionDuration;
-}
-
-.main {
-  display: flex;
-  flex: 1;
-  overflow: hidden;
-}
-
-.input {
-  flex: 1;
-  transition: flex $transitionDuration;
-}
-
-.result {
-  flex: 2;
-  transition: padding-top $transitionDuration;
-}
-
-@media (max-width: 960px) {
-  .main {
-    flex-direction: column;
-  }
-
-  :not(#app.result-investigated) {
-    .input {
-      &.focused {
-        flex: 5;
-      }
-    }
-  }
-
-  .result {
-    border-top: 1px solid #999;
-    flex: 3;
-  }
-
-  #app.result-investigated {
-
-    .header {
-      height: 0;
-      padding-bottom: 0;
-      padding-top: 0;
-    }
-
-    .input {
-      flex: 0;
-    }
-
-    .result {
-      padding-top: 50px;
-    }
-
-  }
-}
-</style>
