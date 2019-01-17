9c4f50c100165932eac668e82575beb7e97e1a28
Tomasz Olędzki <luksfer@poczta.onet.pl>
2019-01-17 18:28:09 +0100

    Fix GA

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import Vue from 'vue';
-import VueAnalytics from 'vue-analytics';
 
 import App from './App.vue';
 // import './registerServiceWorker';
 
 Vue.config.productionTip = false;
-
-Vue.use(VueAnalytics, {
-  id: 'UA-131056476-1',
-});
 
 new Vue({
   render: h => h(App),
